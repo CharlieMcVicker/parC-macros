14ea41fb4fb2a6c9b2b12ec729ca880249d6d51a
Charlie McVicker <charlie@mcvickernet.com>
2026-07-04 13:14:21 -0700

    refactor: Restructure codebase into parc_macros package (TASK-5)

---
+++ parc_macros/__init__.py
@@ -0,0 +1,6 @@
+"""
+parc_macros package.
+"""
+
+from .yaml_validation import validate_yaml_content, validate_yaml_file
+from .generate_markers import parse_csv_with_metadata, main as generate_markers_main
